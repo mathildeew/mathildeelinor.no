c860fb58b62f922a2466f48b214930ad870131bc
Mathilde Wiik <mathildewiik@hotmail.com>
2024-11-05 19:16:35 +0100

    removed router animation on certain paths

--- src/components/ui/Layout/index.jsx
+++ src/components/ui/Layout/index.jsx
@@ -1,22 +1,26 @@
 import { motion as m } from "framer-motion";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 
 export default function Layout() {
+  const location = useLocation();
+  const path = location.pathname;
   return (
     <>
-      <m.div
-        initial={{
-          background: "#002FA7",
-          width: "100vw",
-          height: "100vh",
-          position: "absolute",
-          zIndex: "10",
-        }}
-        animate={{ y: "-100%" }}
-        transition={{ delay: 0.2, duration: 0.5, ease: "easeInOut" }}
-      ></m.div>
+      {path !== "/gjesteboka/logg-inn" && path !== "/gjesteboka/lag-bruker" && (
+        <m.div
+          initial={{
+            background: "#002FA7",
+            width: "100vw",
+            height: "100vh",
+            position: "absolute",
+            zIndex: "10",
+          }}
+          animate={{ y: "-100%" }}
+          transition={{ delay: 0.2, duration: 0.5, ease: "easeInOut" }}
+        ></m.div>
+      )}
       <Header />
       <main className="w-full mx-auto">
         <Outlet />
