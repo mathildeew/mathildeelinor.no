db73e1452f5a44bf2a53afb8c3b4f6865c8888a1
Mathilde Wiik <mathildewiik@hotmail.com>
2024-11-05 15:17:13 +0100

    reduce repitition and moved unused states

--- src/pages/Guestbook/Login/index.jsx
+++ src/pages/Guestbook/Login/index.jsx
@@ -1,15 +1,10 @@
-import { useForm } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
-
-import { yupResolver } from "@hookform/resolvers/yup";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faImages } from "@fortawesome/free-solid-svg-icons";
 import * as yup from "yup";
-import useApi from "../../../hooks/useApi";
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useNavigate, Link } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { getErrorMessage } from "../../../utils/getErrorMessage";
+import useApi from "../../../hooks/useApi";
 import SEOHelmet from "../../../components/SEOHelmet";
-import { useEffect } from "react";
 
 const schema = yup.object({
   name: yup.string().required("Vennligst fyll inn navn").min(3, "Navn må være over 3 tegn").max(30, "Navn må være under 30 tegn"),
@@ -25,24 +20,16 @@ export default function Login() {
     formState: { errors },
   } = useForm({ resolver: yupResolver(schema) });
 
-  const { fetchApi, isLoading, isSuccess, isError, errorMsg } = useApi();
+  const { fetchApi, isLoading, isError, errorMsg } = useApi();
 
   const onSubmit = async (formData) => {
-    const data = {
-      name: formData.name,
-      password: formData.password,
-    };
-
-    const response = await fetchApi(`https://mathildeelinor-gjesteboka.vercel.app/api/login`, "POST", data);
+    const response = await fetchApi(`http://localhost:3000/api/login`, "POST", formData);
 
     if (response.status === 200) {
       window.localStorage.setItem("name", response.data.name);
       window.localStorage.setItem("token", response.data.token);
       window.localStorage.setItem("emoji", response.data.emoji);
-
-      setTimeout(() => {
-        navigate("/gjesteboka");
-      }, 500);
+      navigate("/gjesteboka");
     }
   };
 
@@ -53,20 +40,25 @@ export default function Login() {
       <section className="w-full max-w-md flex flex-col gap-10 mx-auto px-4">
         <form className="flex flex-col gap-6 border-2 py-10 px-2 border-primary md:px-10" onSubmit={handleSubmit(onSubmit)}>
           <h1 className="text-3xl text-center">Logg inn</h1>
+
           <div className="flex flex-col gap-1">
             <label htmlFor="name">Navn</label>
             <input name="name" type="text" className="bg-secondary w-full p-2 border border-primary " {...register("name", { required: true, type: "text" })} />
             <p className="text-red-700">{errors.name?.message}</p>
           </div>
+
           <div className="flex flex-col gap-1">
             <label htmlFor="password">Passord</label>
             <input name="password" type="password" className="bg-secondary w-full p-2 border border-primary " {...register("password", { required: true, type: "text" })} />
             <p className="text-red-700">{errors.password?.message}</p>
           </div>
+
           {isError && <p className="text-red-700">{getErrorMessage(errorMsg)}</p>}
+
           <button type="submit" className="bg-primary text-secondary py-2">
             {isLoading ? "Logger inn..." : "Logg inn"}
           </button>
+
           <div className="flex gap-1">
             <p>Har du ikke en bruker?</p>
             <Link to="/gjesteboka/lag-bruker" className="underline">
