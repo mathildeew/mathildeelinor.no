9c1897bb1f6d7d4a234fbba67373ce3289ab39b5
Mathilde Wiik <mathildewiik@hotmail.com>
2024-11-06 23:10:38 +0100

    adds styles

--- src/components/ui/Layout/Header/index.jsx
+++ src/components/ui/Layout/Header/index.jsx
@@ -24,7 +24,7 @@ export default function Header() {
       <m.nav initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ delay: 0.8, duration: 0.5, ease: "easeInOut" }} className="md-900:relative">
         <FontAwesomeIcon aria-label="Åpne/lukke meny" size="2x" className={`p-1.5 relative top-5 right-5 z-50 transition-all duration-500 ease-in-out ${showMenu && "text-secondary rotate-90"}`} icon={faEllipsis} onClick={() => setShowMenu(!showMenu)} />
 
-        <div className={`bg-primary w-full h-screen absolute right-0 z-40 flex justify-end transition-all duration-500 ease-in-out md-750:w-72 xl-1400:-left-8 ${showMenu ? "top-0 opacity-1" : "-top-[750px] opacity-1"}`}>
+        <div className={`bg-primary w-full h-screen absolute right-0 z-40 flex justify-end transition-all duration-500 ease-in-out md-750:w-72 xl-1400:-left-8 ${showMenu ? "top-0 opacity-1" : "-top-[1000px] opacity-1"}`}>
           <div className="pt-32 pr-8 z-30 flex flex-col gap-16 ">
             <div className="text-end flex flex-col gap-5">
               <NavLink to={"/"} text={"Hjem"} regular={true} />
